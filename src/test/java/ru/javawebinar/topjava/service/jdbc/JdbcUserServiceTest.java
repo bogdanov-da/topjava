c23b440a65dc8a8a66c5476489b8bed2377ecebb
bodm <bodm@gurtam.com>
2024-03-16 15:16:15 +0100

    add jdbc transaction, new user roles

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-import org.junit.Ignore;
+import org.junit.Assume;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
-@Ignore
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
+    @Override
+    public void createWithException() throws Exception {
+        Assume.assumeTrue("Jdbc doesn't accepted", isJpa());
+    }
 }
