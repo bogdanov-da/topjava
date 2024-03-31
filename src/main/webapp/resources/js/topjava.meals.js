b4fd4b6565cf493b5074c4b059acb9c35101e098
bodm <bodm@gurtam.com>
2024-03-31 15:29:01 +0200

    add Meal Ui controller, meals.js

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,38 @@
+const mealAjaxUrl = "profile/meals/";
+
+// https://stackoverflow.com/a/5064235/548473
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+// $(document).ready(function () {
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ]
+        })
+    );
+});
