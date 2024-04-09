e3705c5b20f99e81c7b0a97a7912958dc5b69912
bodm <bodm@gurtam.com>
2024-04-09 12:01:53 +0200

    check excess in meals

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -48,7 +48,10 @@ $(function () {
                     0,
                     "desc"
                 ]
-            ]
+            ],
+            "createdRow": function (row, data, dataIndex) {
+                $(row).attr("data-meal-excess", data.excess);
+            },
         })
     );
 });
